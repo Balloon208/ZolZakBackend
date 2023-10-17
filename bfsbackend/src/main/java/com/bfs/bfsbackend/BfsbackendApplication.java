d9779f353aae27bddc3a7ef67c780d3af708bf11
subak222 <sbag88686@gmail.com>
2023-10-17 10:05:10 +0900

    init

---
+++ bfsbackend/src/main/java/com/bfs/bfsbackend/BfsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.bfs.bfsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfsbackendApplication.class, args);
+	}
+
+}
---
+++ bfsbackend/src/test/java/com/bfs/bfsbackend/BfsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfs.bfsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfsbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
