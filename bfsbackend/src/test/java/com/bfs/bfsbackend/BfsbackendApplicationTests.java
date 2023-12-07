b9876ad9d66fffbb64112e32a2911908a7bc1a5a
Balloon208 <gnswldms2@gmail.com>
2023-12-07 12:28:17 +0900

    Removed : Test

--- bfsbackend/src/test/java/com/bfs/bfsbackend/BfsbackendApplicationTests.java
+++ bfsbackend/src/test/java/com/bfs/bfsbackend/BfsbackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.bfs.bfsbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class BfsbackendApplicationTests {
 
 	@Test
